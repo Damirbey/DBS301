38a9790f967b3a09cdda27d47cb1863fd7929879
Damir <dydamirbey@gmail.com>
2019-02-26 21:10:52 -0500

    DBS 301 (LAB 2)

---
+++ LAB_2.sql
@@ -0,0 +1,11 @@
+-- ***********************
+-- Name: Damirkhon Yodgorov
+-- ID: 108364175
+-- Date: 20.09.2018
+-- Purpose: Lab 2 DBS301
+-- ***********************
+
+
+
+--- Question 1 Display the employee_id, last name and salary of employees earning in the range of
+--- $9,000 to $11,000. Sort the output by top salaries first and then by last name. 
